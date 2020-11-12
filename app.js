12d3f0501943a3a366678de2a8513f20f029e018
Alexandra Faller <alex@faller.us>
2020-11-12 11:14:50 +0000

    re-factored app.js code

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
-const p1Button = document.querySelector("#buttonP1");
-const p2Button = document.querySelector("#buttonP2");
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
+const player1 = {
+ button: document.querySelector("#buttonP1"),
+  display: document.querySelector("#p1Display"),
+  score: 0
+}
+
+const player2 = {
+  button: document.querySelector("#buttonP2"),
+  display: document.querySelector("#p2Display"),
+  score: 0
+}
+
 const winningScoreSelect = document.querySelector("#playTo");
 const resetButton = document.querySelector("#resetButton");
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 5;
 let gameWon = false;
 
 
 
-
-
-p1Button.addEventListener('click', function(){
-    if (!gameWon){
-        p1Score += 1;
-        if (p1Score === winningScore) {
-            gameWon = true;
-            p1Display.classList.add("text-success");
-            p1Button.classList.add("btn-success");
-            p1Button.classList.remove("btn-dark");
-            p2Display.classList.add("text-danger");
-            p2Button.classList.add("btn-danger");
-            p2Button.classList.remove("btn-dark");
-        } 
-        p1Display.textContent = p1Score;
-    }   
-});
-
-p2Button.addEventListener("click", function () {
+function updateScores(player, opponent){
   if (!gameWon) {
-    p2Score += 1;
-    if (p2Score === winningScore) {
+    player.score += 1;
+    if (player.score === winningScore) {
       gameWon = true;
-      p2Display.classList.add("text-success");
-      p2Button.classList.add("btn-success");
-      p2Button.classList.remove("btn-dark");
-      p1Display.classList.add("text-danger");
-      p1Button.classList.add("btn-danger");
-      p1Button.classList.remove("btn-dark");
+      player.display.classList.add("text-success");
+      player.button.classList.add("btn-success");
+      player.button.classList.remove("btn-dark");
+      opponent.display.classList.add("text-danger");
+      opponent.button.classList.add("btn-danger");
+      opponent.button.classList.remove("btn-dark");
     }
-    p2Display.textContent = p2Score;
+    player.display.textContent = player.score;
   }
-});
+}   
+
+player1.button.addEventListener("click", function () {
+  updateScores(player1, player2);
+})
+
+player2.button.addEventListener("click", function () {
+  updateScores(player2, player1);
+})
 
 winningScoreSelect.addEventListener("change", function () {
   winningScore = parseInt(this.value);
@@ -56,15 +52,13 @@ winningScoreSelect.addEventListener("change", function () {
 resetButton.addEventListener("click", reset);
 
 function reset() {
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
+  for (let p of [player1, player2]) {
+    p.score = 0;
+    p.display.textContent = 0;
     gameWon = false;
-    p2Display.classList.remove("text-success", "text-danger");
-    p2Button.classList.remove("btn-success", "btn-danger");
-    p1Display.classList.remove("text-success", "text-danger");
-    p1Button.classList.remove("btn-success", "btn-danger");
-    p1Button.classList.add("btn-dark");
-    p2Button.classList.add("btn-dark");
+    p.display.classList.remove("text-success", "text-danger");
+    p.button.classList.remove("btn-success", "btn-danger");
+    p.button.classList.add("btn-dark");
+    p.button.classList.add("btn-dark");
+  }
 }
